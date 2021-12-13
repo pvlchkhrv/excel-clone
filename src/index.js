9a7d75c0e8b6a4a1d347f834d922abcb26706de6
pvl <pvlchkhrv@gmail.com>
2021-12-13 23:54:08 +0300

    feat: add Dom listeners implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import './scss/index.scss'
+import './scss/index.scss';
+import {Excel} from "@/components/excel/Excel";
+import {Toolbar} from "@/components/toolbar/Toolbar";
+import {Header} from "@/components/header/Header";
+import {Formula} from "@/components/formula/Formula";
+import {Table} from "@/components/table/Table";
 
-console.log('Working!')
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+});
+
+excel.render();
+
+
+
