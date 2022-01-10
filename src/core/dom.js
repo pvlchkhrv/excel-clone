0893300d4d07392fc700be0496c21c08252d65ad
pvl <pvlchkhrv@gmail.com>
2022-01-10 23:01:32 +0300

    feat: add table logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -14,6 +14,17 @@ class Dom {
     return this.$el.outerHTML.trim();
   }
 
+  text(text) {
+    if (typeof text === 'string') {
+      this.$el.textContent = text;
+      return this;
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim();
+    }
+    return this.$el.textContent.trim();
+  }
+
   clear() {
     this.html('');
     return this;
@@ -39,6 +50,10 @@ class Dom {
     return this;
   }
 
+  find(selector) {
+    return $(this.$el.querySelector(selector));
+  }
+
   get data() {
     return this.$el.dataset;
   }
@@ -57,6 +72,32 @@ class Dom {
 
   css(styles = {}) {
     Object.keys(styles).forEach(key => this.$el.style[key] = styles[key]);
+  }
+
+  addClass(className) {
+    this.$el.classList.add(className);
+    return this;
+  }
+
+  removeClass(className) {
+    this.$el.classList.remove(className);
+    return this;
+  }
+
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(':');
+      return {
+        row: +parsed[0],
+        col: +parsed[1]
+      }
+    }
+    return this.data.id;
+  }
+
+  focus() {
+    this.$el.focus();
+    return this;
   }
 }
 
